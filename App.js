82221f1e3ae452c1e0728dd75e5093fe5e28dc83
bachir salhi <designama23@gmail.com>
2025-06-08 13:35:37 +0100

    Add files via upload
    upload src files

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Bienvenue sur mapremière app réact !</h1>
+      <hr/>
+      <p>une application por les lessons prof..!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
